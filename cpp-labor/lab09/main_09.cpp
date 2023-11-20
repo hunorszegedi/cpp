8151f6a8e85f642de7b7a8b129df47e36156833e
hunorszegedi <szegedihunorofficial@gmail.com>
2023-11-20 09:42:24 +0200

    labor 9 orai

--- cpp-labor/lab09/main_09.cpp
+++ cpp-labor/lab09/main_09.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 
+using namespace std;
+
+#include <vector>
+#include <algorithm>
+
 int main() {
-    std::cout << "Hello, Lab_09" << std::endl;
+    vector <string> fruits{
+            "melon", "strawberry", "raspberry", "apple", "banana", "walnut", "lemon"
+    };
+
+
+    //1.
+    cout << *find_if(fruits.begin(), fruits.end(), [](string fruit) {
+        return fruit == "melon";
+    }) << endl;
+
+    //2.
+    vector<int> array;
+    for (int i = 0; i < 10; ++i) {
+        array.push_back(i);
+    }
+    all_of(array.begin(), array.end(), [](int num) {
+        return num % 2 == 0;
+    });
+
+    //3.
+    for_each(array.begin(), array.end(), [&array](int num) {
+        array[num] = num * 2;
+    });
+    for (auto num: array) {
+        cout << num << " ";
+    }
+
+    //4.
+    vector <string> months{
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+    };
+    int counter = 0;
+    cout << endl;
+    for_each(months.begin(), months.end(), [&counter](string month) {
+        if (month.length() > 5) {
+            cout << month << " ";
+            counter++;
+        }
+    });
+    cout << endl << "Counter: " << counter << endl;
+
     return 0;
 }
--- cpp-labor/parcialis_minta/main.cpp
+++ cpp-labor/parcialis_minta/main.cpp
@@ -41,52 +41,52 @@ using namespace std;
 //    return 0;
 //}
 int main() {
-//    Library library(1);
-//    Book b1("Jokai Mor", "Egri csillagok", 1980, 300);
-//    library.addBook(b1);
-//    ofstream output("kimenet.txt");
-//    if (!output.is_open()) {
-//        cerr << "HIBA" << endl;
-//        exit(-1);
-//    }
-//    output << b1 << endl;
-//    output.close();
-//
-//    Book b2("Jokai Mor", "A koszivu ember fiai", 1967, 400);
-//    library.addBook(b2);
-//    Book b3("Murakami Haruki", "Sotetedes utan", 2004, 250);
-//    library.addBook(b3);
-//    Book b4("Murakami Haruki", "A norveg erdo", 1987, 420);
-//    library.addBook(b4);
-//    Book b5("Murakami Haruki", "1Q84", 1987, 800);
-//    library.addBook(b5);
-//    Book b6("Knausgard Karl Ove", "Harcom", 2009, 1000);
-//    library.addBook(b6);
-//
-//    library.printLibrary();
-//
-//    cout << "Find book: " << endl;
-//    string line;
-//    getline(cin, line);
-//    cout << line;
-//    try {
-//        Book bFind = library.findBook(line);
-//        bFind.printBook(cout);
-//    } catch (out_of_range &e) {
-//        cout << e.what() << endl;
-//    }
-//
-//    cout << "Kinek a konyveit keresed?" << endl;
-//    string line2;
-//    getline(cin, line2);
-//    library.countBooks(line2);
-//
-//    library.sortBooks();
-//    library.printLibrary();
-//
-//    library.deleteGivenBook("Sotetedes utan");
-//    library.printLibrary();
-//
-//    vector<int> v = {1, 2, 3, 4};
-//    v.erase(v.begin() + 2);
+    Library library(1);
+    Book b1("Jokai Mor", "Egri csillagok", 1980, 300);
+    library.addBook(b1);
+    ofstream output("kimenet.txt");
+    if (!output.is_open()) {
+        cerr << "HIBA" << endl;
+        exit(-1);
+    }
+    output << b1 << endl;
+    output.close();
+
+    Book b2("Jokai Mor", "A koszivu ember fiai", 1967, 400);
+    library.addBook(b2);
+    Book b3("Murakami Haruki", "Sotetedes utan", 2004, 250);
+    library.addBook(b3);
+    Book b4("Murakami Haruki", "A norveg erdo", 1987, 420);
+    library.addBook(b4);
+    Book b5("Murakami Haruki", "1Q84", 1987, 800);
+    library.addBook(b5);
+    Book b6("Knausgard Karl Ove", "Harcom", 2009, 1000);
+    library.addBook(b6);
+
+    library.printLibrary();
+
+    cout << "Find book: " << endl;
+    string line;
+    getline(cin, line);
+    cout << line;
+    try {
+        Book bFind = library.findBook(line);
+        bFind.printBook(cout);
+    } catch (out_of_range &e) {
+        cout << e.what() << endl;
+    }
+
+    cout << "Kinek a konyveit keresed?" << endl;
+    string line2;
+    getline(cin, line2);
+    library.countBooks(line2);
+
+    library.sortBooks();
+    library.printLibrary();
+
+    library.deleteGivenBook("Sotetedes utan");
+    library.printLibrary();
+
+    vector<int> v = {1, 2, 3, 4};
+    v.erase(v.begin() + 2);
 }
